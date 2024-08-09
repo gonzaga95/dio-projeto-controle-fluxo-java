8bdf80d320c8c382f7e5ca6ab72ff2171610f166
gonzaga95 <gonzagas.dudu@gmail.com>
2024-08-09 13:42:00 -0400

    feat metodo main e metodo contar validando excecao

--- src/edu/dio/principal/Contador.java
+++ src/edu/dio/principal/Contador.java
@@ -1,4 +1,35 @@
 package edu.dio.principal;
 
+import edu.dio.excecao.ParametrosInvalidosException;
+
+import java.util.Scanner;
+
 public class Contador {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = sc.nextInt();
+        System.out.println("Digite o segundo parâmetro");
+        int parametroDois = sc.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException e){
+            System.out.println("O primeiro parâmetro não pode ser menor que o segundo");
+        }
+
+    }
+
+    static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+        if (parametroUm < parametroDois) {
+            throw new ParametrosInvalidosException();
+        }
+
+        int contagem = parametroDois - parametroUm;
+        for (int i = 0; i < contagem; i++) {
+            System.out.println("Imprimindo o número "+ i+1);
+        }
+    }
 }
